f070edf867c2ac35fdd193525ec7654f6286f300
Irina <diva84@mail.ru>
2021-06-19 21:38:15 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+import 'nodelist-foreach-polyfill';
+import 'formdata-polyfill';
+
+// import menuBurger from './modules/menuBurger';
+// import slider from './modules/slider';
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+
+    
+});
